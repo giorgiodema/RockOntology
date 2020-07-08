16cbba58a2a08087c6f242e34c7981ae935eab1e
Giorgio De Magistris <giorgiodema@gmail.com>
2020-07-08 17:17:35 +0200

    artists

--- server.py
+++ server.py
@@ -6,11 +6,14 @@ import os
 import json
 from subprocess import Popen,PIPE
 import hashlib
+import re
 
 app = Flask(__name__)
 
 class Query:
     ABSTRACT = "abstract.rq"
+    ORIGIN = "origin.rq"
+    ARTISTS = "artists.rq"
 
 def generate_uid():
     uid = request.remote_addr + request.user_agent.string
@@ -19,10 +22,11 @@ def generate_uid():
 
 def compile_query(uid,qid,**kwargs):
     out_path = os.path.join("tmp",uid)
-    with open(os.path.join("queries",Query.ABSTRACT),"r") as f:
+    with open(os.path.join("queries",qid),"r") as f:
         t = Template(f.read())
         with open(out_path,"w") as f:
-            f.write(t.render(kwargs))
+            t = t.render(kwargs)
+            f.write(t)
 
 def run_query(uid):
     with Popen([os.path.join("commands","run.bat"),"./tmp/"+uid,"json"], stdout=PIPE) as proc:
@@ -31,7 +35,7 @@ def run_query(uid):
         if res==b'':
             return None
         else:
-            return json.loads(res)
+            return json.loads(res)["results"]["bindings"]
 
 def check_args(*args):
     for a in args:
@@ -56,12 +60,31 @@ def getIndex():
         stylesheet=url_for('static',filename="style.css"))
 
 
+@app.route('/query/artists',methods=['GET'])
+def getArtists():
+
+    result = {"data":[]}
+    if not check_args("genre"):
+        return json.dumps({"error":"invalid arguments"})
+
+    args = get_args("genre")
+    uid = generate_uid()
+
+    # get artists
+    compile_query(uid,Query.ARTISTS,**args)
+    res = run_query(uid)
+    for r in res:
+        a = r["artist"]["value"]
+        match = re.match(r"http://dbpedia.org/resource/(.*)",a)
+        if match and  len(match.groups())==1:
+            result["data"].append(match.groups(1)[0])
+    return result
+
 
 @app.route('/query/info',methods=["GET"])
 def getInfo():
 
     result = {"data":{}}
-
     if not check_args("genre"):
         return json.dumps({"error":"invalid arguments"})
 
@@ -73,10 +96,15 @@ def getInfo():
     compile_query(uid,Query.ABSTRACT,**args)
     res = run_query(uid)
     if res:
-        res = res["results"]["bindings"]
         for r in res:
             if r['info']['xml:lang']=='en':
                 result["data"]["abstract"] = r['info']['value']
+    
+    # get origin
+    compile_query(uid,Query.ORIGIN,**args)
+    res = run_query(uid)
+    if res and len(res)>0:
+        result["data"]["origin"] = int(float(res[0]["origin"]["value"]))
 
     return json.dumps(result)
 

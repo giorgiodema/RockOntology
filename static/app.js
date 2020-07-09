fa4f28b9283bfdd39ed6691a42d6800c727b6c69
Giorgio De Magistris <giorgiodema@gmail.com>
2020-07-09 17:28:28 +0200

    show hide popup

--- static/app.js
+++ static/app.js
@@ -1,3 +1,16 @@
 document.body.onload = function(){
+
+    /* initialize components */
+    var div_genre_rock_music = document.getElementById("div_genre_rock_music")
+    var popup_genre_rock_music = document.getElementById("popup_genre_rock_music")
+
+    /* attach handlers */
+    div_genre_rock_music.onclick = function(){
+        if(popup_genre_rock_music.style.visibility=="collapse")
+            popup_genre_rock_music.style.visibility="visible"
+        else
+            popup_genre_rock_music.style.visibility="collapse"
+    }
+
 }
 

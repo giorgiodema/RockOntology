e26118722f1e534639d0affc5eed66a87084dbde
giorgiodema <giorgiodema@gmail.com>
2020-07-10 16:29:10 +0200

    info response listener

--- static/app.js
+++ static/app.js
@@ -1,6 +1,12 @@
+var current_genre = null
+
 var MusicGenre = function(name,origin){
     this.name = name
     this.origin = origin
+    this.sub_genres = []
+    this.fus_genres = []
+    this.artists = []
+    this.groups = []
 }
 MusicGenre.prototype.draw = function(){
     /*
@@ -25,57 +31,62 @@ MusicGenre.prototype.draw = function(){
     /* attach new items to the html page
     */
     document.getElementById('main_container').innerHTML += container + popup
-    /*
-    /*  add event listeners to the new
-    /*  added elements 
-    */
-    document.addEventListener('click',function(e){
+}
+
+function clickListeners(e){
         /*
         /*  click listener on the genre container
         /*  the listner shows the corresponding 
         /*  popup menu 
         */
-        if(e.target && e.target.className=="item_genre_container"){
-            var popup = document.getElementById("popup_genre_"+e.target.getAttribute("name"))
-            if(popup.style.visibility=="collapse")
-                popup.style.visibility="visible"
-            else
-                popup.style.visibility="collapse"
-        }
-        /*
-        /*  listeners for the items of the popup menu:
-        /*  -> INF: display the long description of the genre
-        /*          and the image
-        /*  -> SUB: display all the subgenres of the
-        /*          current genre
-        /*  -> FUS: display all the fusion genres
-        /*          with the current genre
-        /*  -> ART: display all the artists of the
-        /*          current genre
-        /*  -> GRO: display all the groups of the current genre
-        */
-        if(e.target && (e.target.className=="genre_inf" || e.target.parentElement.className=="genre_inf")){
-            console.log("genre_inf")
-        }
-        if(e.target && (e.target.className=="genre_sub" || e.target.parentElement.className=="genre_sub")){
-            console.log("genre_sub")
-        }
-        if(e.target && (e.target.className=="genre_fus" || e.target.parentElement.className=="genre_fus")){
-            console.log("genre_fus")
-        }
-        if(e.target && (e.target.className=="genre_art" || e.target.parentElement.className=="genre_art")){
-            console.log("genre_art")
-        }
-        if(e.target && (e.target.className=="genre_gro" || e.target.parentElement.className=="genre_gro")){
-            console.log("genre_gro")
-        }
+       if(e.target && e.target.className=="item_genre_container"){
+        var popup = document.getElementById("popup_genre_"+e.target.getAttribute("name"))
+        if(popup.style.visibility=="collapse")
+            popup.style.visibility="visible"
+        else
+            popup.style.visibility="collapse"
+    }
+    /*
+    /*  listeners for the items of the popup menu:
+    /*  -> INF: display the long description of the genre
+    /*          and the image
+    /*  -> SUB: display all the subgenres of the
+    /*          current genre
+    /*  -> FUS: display all the fusion genres
+    /*          with the current genre
+    /*  -> ART: display all the artists of the
+    /*          current genre
+    /*  -> GRO: display all the groups of the current genre
+    */
+    if(e.target && (e.target.className=="genre_inf" || e.target.parentElement.className=="genre_inf")){
+        console.log("genre_inf")
+        var oReq = new XMLHttpRequest()
+        oReq.addEventListener("load", infoResponseListener)
+        oReq.open("GET", document.location.origin + "/query/genre/info"+"?"+"genre="+current_genre.name)
+        oReq.send()
+    }
+    if(e.target && (e.target.className=="genre_sub" || e.target.parentElement.className=="genre_sub")){
+        console.log("genre_sub")
+    }
+    if(e.target && (e.target.className=="genre_fus" || e.target.parentElement.className=="genre_fus")){
+        console.log("genre_fus")
+    }
+    if(e.target && (e.target.className=="genre_art" || e.target.parentElement.className=="genre_art")){
+        console.log("genre_art")
+    }
+    if(e.target && (e.target.className=="genre_gro" || e.target.parentElement.className=="genre_gro")){
+        console.log("genre_gro")
+    }
+}
 
-    })
+function infoResponseListener(e){
+    console.log(this.responseText)
 }
 
 
 document.body.onload = function(){
-    var rock_music = new MusicGenre("Rock_music","1960")
-    rock_music.draw()
+    document.addEventListener('click',clickListeners)
+    current_genre = new MusicGenre("Rock_music","1960")
+    current_genre.draw()
 }
 

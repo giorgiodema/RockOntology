b3a01e7d1dc537d1695848ae149fb3b6f32cd4ca
giorgiodema <giorgiodema@gmail.com>
2020-07-10 13:38:45 +0200

    attach elements dynamically

--- static/app.js
+++ static/app.js
@@ -1,16 +1,34 @@
+var MusicGenre = function(name,origin){
+    this.name = name
+    this.origin = origin
+}
+MusicGenre.prototype.draw = function(){
+    var container =     '<div class="item_genre_container" id="div_genre_'+this.name+'" name='+this.name+'>\
+                            <p class="genre_title">'+this.name.replace("_"," ")+'</p>\
+                            <p class="genre_origin">'+this.origin+'</p>\
+                        </div>'
+    var popup = '       <ul class=popup_genre id="popup_genre_'+this.name+'">\
+                            <li><p>More Info</p></li>\
+                            <li><p>Subgenres</p></li>\
+                            <li><p>Fusion Genres</p></li>\
+                            <li><p>Artists</p></li>\
+                            <li><p>Groups</p></li>\
+                        </ul>'
+    document.getElementById('main_container').innerHTML += container + popup
+    document.addEventListener('click',function(e){
+        if(e.target && e.target.className=="item_genre_container"){
+            var popup = document.getElementById("popup_genre_"+e.target.getAttribute("name"))
+            if(popup.style.visibility=="collapse")
+                popup.style.visibility="visible"
+            else
+                popup.style.visibility="collapse"
+        }
+    })
+}
+
+
 document.body.onload = function(){
-
-    /* initialize components */
-    var div_genre_rock_music = document.getElementById("div_genre_rock_music")
-    var popup_genre_rock_music = document.getElementById("popup_genre_rock_music")
-
-    /* attach handlers */
-    div_genre_rock_music.onclick = function(){
-        if(popup_genre_rock_music.style.visibility=="collapse")
-            popup_genre_rock_music.style.visibility="visible"
-        else
-            popup_genre_rock_music.style.visibility="collapse"
-    }
-
+    var rock_music = new MusicGenre("Rock_music","1960")
+    rock_music.draw()
 }
 
